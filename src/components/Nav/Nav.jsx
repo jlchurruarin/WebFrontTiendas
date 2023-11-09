1232f12c4ae8fead0f75df13fd54509c9edf2516
leandro-sarvi <leandromartinsarvi@hotmail.com>
2023-11-09 14:27:25 -0300

    map tiendas

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 import { NavLink } from "react-router-dom";
 import megaLogo from  '../../assets/mega.png'
 import styles from '../Nav/Nav.module.css';
@@ -34,6 +35,7 @@ function Nav(){
                     Comandos POS
                   </NavLink>
                 </li>
+                <li><LocationOnIcon/><a href="https://www.google.com/maps/d/u/0/viewer?mid=1fROX-tIf7FeQf03IzBbFN0D2bhj5A_s&ll=-31.74198896616553%2C-55.02777490954791&z=4" target="_blank">MapTiendas</a></li>
                 <li><a className={styles.button} href="https://sites.google.com/gdnargentina.com/mdamegatech/instructivos" target="_blank">Instructivos</a></li>
           </ul>
         </nav>
