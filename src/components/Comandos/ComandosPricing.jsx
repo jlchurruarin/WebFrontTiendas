cf9ab35dd07bb7e6294fbc2b8726189f1c415b80
José Luis Churruarin <75645175+jlchurruarin@users.noreply.github.com>
2024-10-14 14:24:48 -0300

    Nuevo comando para validar PMT completo

--- src/components/Comandos/ComandosPricing.jsx
+++ src/components/Comandos/ComandosPricing.jsx
@@ -52,7 +52,6 @@ function ComandosPricing() {
     if(value==""){
        alert("Ingrese UPC/Texto")
     }else{
-      //navigator.clipboard.writeText(`grep ${value} /root/SKYNET/dpfile3/FILE.PMT | awk -F "," '{print "\\n\\x1b[32m******************* VERIFICAR PROMOCIONES *******************\\n\\n\\x1b[0m\\n******************* ID PROMOCIÓN ******************* \\n\\n - "$3"\\n\\n******************* NOMBRE ******************* \\n\\n - \\x1b[33m"$5"\\x1b[0m\\n\\n - Fecha de inicio: "substr($6, 7, 2)"-"substr($6, 5, 2)"-"substr($6, 1, 4)" "$7"\\n - Fecha de fin: "substr($8, 7, 2)"-"substr($8, 5, 2)"-"substr($8, 1, 4)" "$9;if($44 ~ /[[:space:]]/) print "\\x1b[31m - LA PROMOCIÓN TIENE ESPACIOS\\x1b[0m"; else print "\\x1b[32m - LA PROMOCIÓN NO TIENE ESPACIOS\\x1b[0m"; print "\\n\\n******************* LISTA DE TIENDAS ******************* \\n\\n - "$25; print "\\n\\n\\x1b[32m******************** FIN *******************\\x1b[0m" }'`);
       navigator.clipboard.writeText(`grep ${value} /root/SKYNET/dpfile3/FILE.PMT | awk -F "," '{print "\\n\\x1b[32m******************* VERIFICAR PROMOCIONES *******************\\n\\n\\x1b[0m\\n******************* ID PROMOCIÓN ******************* \\n\\n - "$3"\\n\\n******************* NOMBRE ******************* \\n\\n - \\x1b[33m"$5"\\x1b[0m\\n\\n - Fecha de inicio: "substr($6, 7, 2)"-"substr($6, 5, 2)"-"substr($6, 1, 4)" "$7"\\n - Fecha de fin: "substr($8, 7, 2)"-"substr($8, 5, 2)"-"substr($8, 1, 4)" "$9;if($44 ~ /[[:space:]]/) print "\\x1b[31m - LA PROMOCIÓN TIENE ESPACIOS\\x1b[0m"; else print "\\x1b[32m - LA PROMOCIÓN NO TIENE ESPACIOS\\x1b[0m"; if($44 ~ /[\\.\\.\\.]/) print "\\x1b[31m - LA PROMOCIÓN TIENE PUNTOS EN LOS UPC\\x1b[0m"; print "\\n\\n******************* LISTA DE TIENDAS ******************* \\n\\n - "$25; print "\\n\\n\\x1b[32m******************** FIN *******************\\x1b[0m"}'`);
     }
      
@@ -78,6 +77,7 @@ function ComandosPricing() {
     "service epson_devicecontrollogserviced status\nservice epson_pcsvcd status\nservice epson_pcsvcd restart\nservice epson_devicecontrollogserviced restart\n",
     "service Wildfly status\nservice Wildfly restart\nservice ArsPluMnt status\nservice ArsPluMnt restart\nservice POSServerRest status\nservice POSServerRest restart\nservice UserWatcher status\nservice UserWatcher restart",
     "dmidecode -t system", "service --status-all","pos ws-list","cd /root/SKYNET/dpfile3/ && ll",'cd "/home/reg/DP/NCR/DigitalPromotions/Promotion Files" && ll']
+  const commandValidatePMT = `cat /root/SKYNET/dpfile3/FILE.PMT | awk -F "," '{ if($44 ~ /[[:space:]]/) { print "LA PROMOCION $3 TIENE ESPACIOS\x1b[0m"; print "UPC: "; system("grep -o \"[0-9]\\{10,13\\}\" <<< \"" $44 "\"") }}'`
   useEffect(() => {
     const timer = setTimeout(() => {
       setloading(false);
@@ -129,11 +129,20 @@ function ComandosPricing() {
 <div className={styles.h3SubTitle}>
 <h3 className={styles.subTitle}>Verificar PMT</h3>
 <div className={styles.grid}>
-<div className={styles.divCard}>
+          <div className={styles.divCard}>
             <p className={styles.nameComando}>PMT en ARS</p>
             <div className={styles.card}>
               <input disabled className={styles.textInput} value={comandos[10]}></input>
               <button className={styles.btnCopy} onClick={() => copy(comandos[10])} title="copy">
+                <ContentCopyIcon sx={{ color: "#c06500" }} />
+              </button>
+            </div>
+          </div>
+          <div className={styles.divCard}>
+            <p className={styles.nameComando}>Validar PMT Completo en ARS</p>
+            <div className={styles.card}>
+              <input disabled className={styles.textInput} value={commandValidatePMT}></input>
+              <button className={styles.btnCopy} onClick={() => copy(commandValidatePMT)} title="copy">
                 <ContentCopyIcon sx={{ color: "#c06500" }} />
               </button>
             </div>
@@ -146,9 +155,9 @@ function ComandosPricing() {
                 <ContentCopyIcon sx={{ color: "#c06500" }} />
               </button>
             </div>
-          </div>
-          </div>
-          </div>
+  </div>
+  </div>
+  </div>
 
  
   
