7e69e461c6682f30fa625aef23e264d0714e8c38
José Churruarin <jlchurruarin@gmail.com>
2026-01-08 12:50:50 -0300

    Se agregan comando de verificación básica de pinpad
    * Desconexiones de pinpad
    * Tipo de USB
    * Configuraciones tty
    * Error de lib.log
    * Trx Pendientes

--- src/components/Comandos/Comandos.jsx
+++ src/components/Comandos/Comandos.jsx
@@ -23,7 +23,14 @@ function Comandos() {
       ) : (
         <div className={styles.mainContent}>
           <div className={styles.container}>
-          <CommandCategory title="ARS - Servidor de tienda">
+            <CommandCategory title="Verificaciones básicas de pinpad">
+              <Command commandName="* Desconexiones de pinpad" placeHolder="Ver desconexiones de pinpad" command={"grep -E \"New USB device found.*idVendor=11ca, idProduct=022d\" /var/log/messages | awk '{count[$1 \" \" $2]++} END {for (d in count) print d \": \" count[d]}' | sort"}/>
+              <Command commandName="* Tipo de USB" placeHolder="lsusb -t" command="lsusb -t" helpText='Muestra información del puerto en donde se encuentra conectado cada dispositivo usb'/>
+              <Command commandName="* Ver configuración tty" placeHolder="stty -F /dev/ttyS20 -a" command="stty -F /dev/ttyS20 -a" helpText="Permite ver la configuraciones de conexión entre la POS y Pinpad"/>
+              <Command commandName="* Errores lib.log" placeHolder="Ver errores lib.log" command={"grep \"ERROR\" $(awk '/cd / {print $2}' /etc/init.d/emv)/log/lib.log"}/>
+              <Command commandName="* Trx pendientes" placeHolder="Ver transaciones pendientes en pinpad" command={"grep \"Trxs Pendientes\" $(awk '/cd / {print $2}' /etc/init.d/emv)/log/lib.log"}/>
+            </CommandCategory>
+            <CommandCategory title="ARS - Servidor de tienda">
               <Command commandName="* Estado linea de cajas" placeHolder="/home/WMAR/check_POS.sh" command="/home/WMAR/check_POS.sh" helpUrl="https://drive.google.com/file/d/1QY20SzvNonbXEY7cfhNSAx6Ekp4DiheB/edit"/>
               <Command commandName="* Listar de cajas +info" placeHolder="pos ws-list" command="pos ws-list"/>
               <Command commandName="* Estado servicios ARS" placeHolder="service --status-all" command="service --status-all"/>
